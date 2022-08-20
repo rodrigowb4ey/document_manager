9efbd077ef5cf61a3eea6a7ae95256a774d6abbd
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2022-08-20 10:29:45 -0300

    Created FolderSerializer

---
+++ folder/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from folder.models import Folder
+
+
+class FolderSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.HyperlinkedRelatedField(
+        queryset=User.objects.all(), view_name="user-detail"
+    )
+
+    class Meta:
+        model = Folder
+        fields = [
+            "url",
+            "uuid",
+            "name",
+            "owner",
+            "created_at",
+        ]
