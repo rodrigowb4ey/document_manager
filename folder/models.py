49fa95ce1d750a3a76b83556705d5e5ce5016e73
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2022-08-20 10:15:56 -0300

    Created 'folder' app + 'Folder' model + migrations

--- folder/models.py
+++ folder/models.py
@@ -3,28 +3,17 @@ from uuid import uuid4
 from django.contrib.auth.models import User
 from django.db import models
 
-from folder.models import Folder
 
-
-class Document(models.Model):
-    class Meta:
-        verbose_name = "Document"
-        verbose_name_plural = "Documents"
-
+class Folder(models.Model):
     uuid = models.UUIDField(default=uuid4, primary_key=True, editable=False)
     name = models.TextField(max_length=80)
-    content = models.FileField(upload_to="%Y/%m/%d")
-    folder = models.ForeignKey(
-        Folder, on_delete=models.CASCADE, blank=True, null=True
-    )
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
 
     def __repr__(self):
         return (
-            f"Document(name={self.name}, "
+            f"Folder(name={self.name}, "
             f"owner={self.owner}, "
-            f"content={self.content}, "
             f"created_at={self.created_at}, "
             f"uuid={self.uuid})"
         )
