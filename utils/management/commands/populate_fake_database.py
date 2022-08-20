355638071f68d7f2c9343921042c380b70e7073f
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2022-08-20 12:20:00 -0300

    Refactored populate_fake_database command to set the number of folders and documents

--- utils/management/commands/populate_fake_database.py
+++ utils/management/commands/populate_fake_database.py
@@ -1,14 +1,29 @@
 from django.core.management.base import BaseCommand
+
 from document.factories import DocumentFactory
+from folder.factories import FolderFactory
 
 
 class Command(BaseCommand):
     help = "Populating fake database for development"
 
     def add_arguments(self, parser):
-        parser.add_argument('num_of_instances', type=int, nargs='?', default=10)
-    
+        parser.add_argument(
+            "--folders", type=int, help="Indicate number of folders"
+        )
+
+        parser.add_argument(
+            "--documents", type=int, help="Indicate number of documents"
+        )
+
     def handle(self, *args, **kwargs):
-        num_of_instances = kwargs.get('num_of_instances', None)
-        DocumentFactory.create_batch(num_of_instances)
-        print(f"Populating fake database!")
+        if kwargs["folders"] and kwargs["documents"]:
+            num_of_folders = kwargs["folders"]
+            num_of_documents = kwargs["documents"]
+            FolderFactory.create_batch(num_of_folders)
+            DocumentFactory.create_batch(num_of_documents)
+        else:
+            FolderFactory.create_batch(5)
+            DocumentFactory.create_batch(25)
+
+        print("Populating fake database!")
