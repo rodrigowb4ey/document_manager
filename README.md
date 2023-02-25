2c3a5a04db58115888d7c5af2e806288778dd5d4
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2023-02-25 09:21:59 -0300

    refactor: Fixing build dependencies and moving backend project files to 'backend' folder chore: Updating .gitignore build: updating Dockerfile and docker-compose build: Updating project settings build: Updating dev dependencies build: Updating cryptography package due to github alert build: Updating oauthlib dependency due to github alert chore: extending 501 code on flake8 chore: Formatting code with blue fix: Fixing entrypoint script chore: Moving backend project files to 'backend' folder

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # document_manager
-Study project using Django with Django REST Framework &amp; Vue
+Back-end for Document Manager Project
+
+### Built with
+
+- [Django](https://www.djangoproject.com/)
+- [Django REST Framework](https://www.django-rest-framework.org/)
+- [PostgreSQL](https://www.postgresql.org/)
+- [Docker](https://www.docker.com/)
+
+## Project Setup for Development
+
+```sh
+docker compose build
+docker compose up
+```
+
+## Author
+
+- Rodrigo Bezerra Saraiva
---
+++ backend/document/tests/test_models.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import User
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase
+
+from document.models import Document
+
+
+class DocumentModelTestCase(TestCase):
+    def setUp(self):
+        self.test_user = User(
+            username='testuser',
+            email='user@test.com',
+            is_superuser=True,
+        )
+
+        self.test_file = SimpleUploadedFile('test_file.pdf', b'test content')
+
+        self.test_document = Document(
+            name='Test Document',
+            content=self.test_file,
+            owner=self.test_user,
+        )
+
+    def test_valid_data_creates_document_instance(self):
+        self.assertTrue(self.test_document)
+        self.assertEqual(self.test_document.name, 'Test Document')
+        self.assertEqual(self.test_document.owner.username, 'testuser')
+        self.assertEqual(self.test_document.content, self.test_file)
+
+    def test_document_repr_method(self):
+        self.assertEqual(
+            repr(self.test_document),
+            (
+                f'Document(name={self.test_document.name}, '
+                f'owner={self.test_document.owner}, '
+                f'content={self.test_document.content}, '
+                f'created_at={self.test_document.created_at}, '
+                f'uuid={self.test_document.uuid})'
+            ),
+        )
+
+    def test_document_str_method(self):
+        self.assertEqual(str(self.test_document), self.test_document.name)
